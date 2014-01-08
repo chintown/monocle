b658a172dc9a03933bb4b117cdc0ddf88dcc948c
Mike Chen <chintown@gmail.com>
2014-01-08 23:37:14 +0800

    fix missing column

--- htdoc/index.php
+++ htdoc/index.php
@@ -3,8 +3,8 @@
     $w = $_GET['w'];
     $h = $_GET['h'];
     $url = urldecode($_GET['u']);
-    $path = 'export PATH=/usr/bin/';
+    $path = 'export PATH=/usr/bin/;';
     $cmd = $path."casperjs /home/chintown/src/chrome/monocle/tools/rasterize_casper.js '$url' $w $h";
-    echo $cmd;
     exec($cmd, $result);
-    var_dump($result);
+    //echo $cmd;
+    //var_dump($result);
