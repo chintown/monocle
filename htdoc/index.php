12b5d24a98dc9a8ac78952630e6f41b29d6f65b4
Mike Chen <chintown@gmail.com>
2014-01-08 19:26:07 +0800

    update server-side snapshot

--- htdoc/index.php
+++ htdoc/index.php
@@ -3,8 +3,8 @@
     $w = $_GET['w'];
     $h = $_GET['h'];
     $url = urldecode($_GET['u']);
-    $path = 'export PATH=/usr/bin/:/usr/local/bin/:/Users/chintown/opt/;';
-    $cmd = $path."casperjs /Users/chintown/src/chrome/monocle/tools/rasterize_casper.js '$url' /tmp/foo.jpg $w $h";
+    $path = 'export PATH=/usr/bin/';
+    $cmd = $path."casperjs /home/chintown/src/chrome/monocle/tools/rasterize_casper.js '$url' $w $h";
     echo $cmd;
     exec($cmd, $result);
     var_dump($result);
--- tools/rasterize_casper.js
+++ tools/rasterize_casper.js
@@ -1,14 +1,20 @@
 // https://github.com/tutsplus/responsive-screenshots-casper/blob/master/casperscreen.js
 var casper = require("casper").create(),
     url = casper.cli.args[0],
-    saveDir = url.replace(/[^a-zA-Z0-9]/gi, '-').replace(/^https?-+/, '');
+    filename = url.replace(/[^a-zA-Z0-9]/gi, '-').replace(/^https?-+/, '');
 
 casper.start();
-var filename = casper.cli.args[1],
-    width = casper.cli.args[2],
-    height = casper.cli.args[3];
+// var filename = casper.cli.args[1],
+var width = casper.cli.args[1];
+var height = casper.cli.args[2];
+
 //give some time for the page to load
 casper.wait(5000, function() {
+    // this.echo(url);
+    // this.echo(filename);
+    // this.echo(width);
+    // this.echo(height);
+
     //set the viewport to the desired height and width
     this.viewport(width, height);
     casper.thenOpen(url, function() {
@@ -16,12 +22,14 @@ casper.wait(5000, function() {
         //Capture selector captures the whole body
         // this.captureSelector(filename, 'body');
         //capture snaps a defined selection of the page
+        filename = '/tmp/monocle_' + filename+'.jpg';
+        //filename = 'abc.jpg';
         this.capture(
             filename,
             {top: 0,left: 0,width: width, height: height},
-            {format: 'jpg',quality: 40}
+            {format: 'jpg',quality: 20}
         );
-        this.echo('snapshot taken');
+        this.echo('snapshot taken '+filename);
     });
 });
 
